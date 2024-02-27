0be3ed68f84dd67ada6569e03e6456d6490e6611
Hood-fr <36336734+Hood-fr@users.noreply.github.com>
2024-02-27 13:35:07 +0100

    Released 14.a

--- main.inc.php
+++ main.inc.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Check Uploads
-Version: auto
+Version: 14.a
 Description: Check upload directory for unexpected or missing files
-Plugin URI: http://piwigo.org/ext/extension_view.php?eid=
+Plugin URI: http://piwigo.org/ext/extension_view.php?eid=923
 Author: plg
 Author URI: http://piwigo.org
 Has Settings: webmaster
